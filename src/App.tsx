9848acdea66641d991997865e068b87f450b16f5
mania125 <alsgurmaniadev@gmail.com>
2020-04-29 20:15:58 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import Greetings from './Greetings';
+// import Greetings from './Greetings';
+// import Counter from './Counter';
+import MyForm from './MyForm';
 
 function App() {
-  const onClick = (name: string) => {
-    console.log(name);
+
+  // const onClick = (name: string) => {
+  //   console.log(name);
+  // }
+
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
   }
 
   return (
-    <Greetings name='Jack' optional='야호' onClick={onClick} />
+    // <Greetings name='Jack' optional='야호' onClick={onClick} />
+    // <Counter />
+    <MyForm onSubmit={onSubmit}/>
   );
 }
 
